6d99a3aa6ad46e8ed89c8b4df550229b42d0564d
Connor Dewar <connor.dewar@gmail.com>
2014-05-01 11:32:59 +1200

    Prettify'd the slideshow and slowed down the animations and the time for each slide.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
 	$("#slider").easySlider({
 		auto: true,
 		continuous: true ,
-		controlsShow: false
+		controlsShow: false,
+		speed: 2000,
+		pause: 3000
 
 	});
 
 });
+
+//this javascript was written by laird ""uber skux"" morris and Connor ""super swag "" Dewar.
+//Cause we are the best
+//and yolo and stuff
