e4c5f484828789bb02179a5484f4033d6a88784b
Connor Dewar <connor.dewar@gmail.com>
2014-05-01 12:54:57 +1200

    Added cool thumbnail popup thingy

--- js/main.js
+++ js/main.js
@@ -8,6 +8,43 @@ $(document).ready(function(){
 
 	});
 
+	$("#cell1").click(function(){
+	window.open('imgpopup.html?test=1', 'Display The image', "height=400, width=990");
+	});
+	$("#cell2").click(function(){
+	window.open('imgpopup.html?test=2', 'Display The image', "height=400, width=990");
+	});
+	$("#cell3").click(function(){
+	window.open('imgpopup.html?test=3', 'Display The image', "height=400, width=990");
+	});
+	$("#cell4").click(function(){
+	window.open('imgpopup.html?test=4', 'Display The image', "height=400, width=990");
+	});
+	$("#cell5").click(function(){
+	window.open('imgpopup.html?test=5', 'Display The image', "height=400, width=990");
+	});
+	$("#cell6").click(function(){
+	window.open('imgpopup.html?test=6', 'Display The image', "height=400, width=990");
+	});
+	$("#cell7").click(function(){
+	window.open('imgpopup.html?test=7', 'Display The image', "height=400, width=990");
+	});
+	$("#cell8").click(function(){
+	window.open('imgpopup.html?test=8', 'Display The image', "height=400, width=990");
+	});
+	$("#cell9").click(function(){
+	window.open('imgpopup.html?test=9', 'Display The image', "height=400, width=990");
+	});
+	$("#cell10").click(function(){
+	window.open('imgpopup.html?test=10', 'Display The image', "height=400, width=990");
+	});
+	$("#cell11").click(function(){
+	window.open('imgpopup.html?test=11', 'Display The image', "height=400, width=990");
+	});
+	$("#cell12").click(function(){
+	window.open('imgpopup.html?test=12', 'Display The image', "height=400, width=990");
+	});
+
 });
 
 //this javascript was written by laird ""uber skux"" morris and Connor ""super swag "" Dewar.
