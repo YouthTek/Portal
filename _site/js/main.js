8a0f25b63a3419814dd5da46050689d8c1340720
Connor Dewar <connor.dewar@gmail.com>
2014-05-01 15:49:54 +1200

    More prettying of slideshow and image table

--- _site/js/main.js
+++ _site/js/main.js
@@ -8,41 +8,43 @@ $(document).ready(function(){
 
 	});
 
+	$("#thumbtab").height($("#cont").height());
+
 	$("#cell1").click(function(){
-	window.open('imgpopup.html?test=1', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=1', 'Display The image', "height=410, width=1000");
 	});
 	$("#cell2").click(function(){
-	window.open('imgpopup.html?test=2', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=2', 'Display The image', "height=410, width=1000");
 	});
 	$("#cell3").click(function(){
-	window.open('imgpopup.html?test=3', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=3', 'Display The image', "height=410, width=1000");
 	});
 	$("#cell4").click(function(){
-	window.open('imgpopup.html?test=4', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=4', 'Display The image', "height=410, width=1000");
 	});
 	$("#cell5").click(function(){
-	window.open('imgpopup.html?test=5', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=5', 'Display The image', "height=410, width=1000");
 	});
 	$("#cell6").click(function(){
-	window.open('imgpopup.html?test=6', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=6', 'Display The image', "height=410, width=1000");
 	});
 	$("#cell7").click(function(){
-	window.open('imgpopup.html?test=7', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=7', 'Display The image', "height=410, width=1000");
 	});
 	$("#cell8").click(function(){
-	window.open('imgpopup.html?test=8', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=8', 'Display The image', "height=410, width=1000");
 	});
 	$("#cell9").click(function(){
-	window.open('imgpopup.html?test=9', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=9', 'Display The image', "height=410, width=1000");
 	});
 	$("#cell10").click(function(){
-	window.open('imgpopup.html?test=10', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=10', 'Display The image', "height=410, width=1000");
 	});
 	$("#cell11").click(function(){
-	window.open('imgpopup.html?test=11', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=11', 'Display The image', "height=410, width=1000");
 	});
 	$("#cell12").click(function(){
-	window.open('imgpopup.html?test=12', 'Display The image', "height=400, width=990");
+	window.open('imgpopup.html?test=12', 'Display The image', "height=410, width=1000");
 	});
 
 });
